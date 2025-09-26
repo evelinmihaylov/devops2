a26b231c1b05d8ff8d3e350eec911723f784de7f
evelin <40690984@live.napier.ac.uk>
2025-09-26 23:27:21 +0100

    Commited

--- src/main/java/com/napier/sem/Main.java
+++ src/main/java/com/napier/sem/Main.java
@@ -1,30 +1,35 @@
-
 package com.napier.sem;
 
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoDatabase;
 import com.mongodb.client.MongoCollection;
 import org.bson.Document;
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-// Connect to MongoDB on local system - we're using port 27000
-        MongoClient mongoClient = new MongoClient("localhost", 27000);
-        // Get a database - will create when we use it
-        MongoDatabase database = mongoClient.getDatabase("mydb");
-        // Get a collection from the database
-        MongoCollection<Document> collection = database.getCollection("test");
-        // Create a document to store
-        Document doc = new Document("name", "Kevin Sim")
-                .append("class", "DevOps")
-                .append("year", "2024")
-                .append("result", new Document("CW", 95).append("EX", 85));
-        // Add document to collection
-        collection.insertOne(doc);
+        // Use try-with-resources so MongoClient is closed automatically
+        try (MongoClient mongoClient = new MongoClient("localhost", 27000)) {
+            // Get a database - will create when we use it
+            MongoDatabase database = mongoClient.getDatabase("mydb");
+            // Get a collection from the database
+            MongoCollection<Document> collection = database.getCollection("test");
 
-        // Check document in collection
-        Document myDoc = collection.find().first();
-        System.out.println(myDoc.toJson());
+            // Create a document to store
+            Document doc = new Document("name", "Kevin Sim")
+                    .append("class", "DevOps")
+                    .append("year", "2024")
+                    .append("result", new Document("CW", 95).append("EX", 85));
+
+            // Add document to collection
+            collection.insertOne(doc);
+
+            // Check document in collection
+            Document myDoc = collection.find().first();
+            if (myDoc != null) {
+                System.out.println(myDoc.toJson());
+            } else {
+                System.out.println("No document found!");
+            }
         }
     }
+}
